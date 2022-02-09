eea6f1ba9e6f010c6781366225e0666c1237ee54
LukesterWad <65312075+larsystem@users.noreply.github.com>
2022-02-09 19:22:38 +0000

    feat: Zones color averages can be accessed
    Each zone now has an object of the ImageHandler class. This new class
    has methods to update the image and get the colour average of the
    image. To avoid redundantly storing the coordinate values from the Zone
    class, only the image width and height are stored as they are needed to
    iterate through coordinate values in the image. The Zone class also has
    two new methods: setImage() for cropping and storing the image in the
    image handler and getColor() for accessing the average color of the
    image. Additionally, a type "clr" has been defined as a tuple of three
    integers to enhance code readability.

--- Zone.py
+++ Zone.py
@@ -1,3 +1,33 @@
+from PIL.Image import Image
+
+clr = tuple[int, int, int]
+
+
+class ImageHandler:
+    def __init__(self, zone_width: int, zone_height: int):
+        self.__WIDTH = zone_width
+        self.__HEIGHT = zone_height
+        self.__TOTAL = zone_width * zone_height
+
+    def setImage(self, image: Image): self.__image = image
+
+    def getAverage(self) -> clr:
+        r = g = b = 0
+        # Iterate through each pixel in the image and sum their RGB values.
+        for x in range(0, self.__WIDTH):
+            for y in range(0, self.__HEIGHT):
+                pixel = self.__image.getpixel((x, y))
+                r += pixel[0]
+                g += pixel[1]
+                b += pixel[2]
+        # Return the mean of the color values for all the pixels in the image.
+        return (
+            r // self.__TOTAL,
+            g // self.__TOTAL,
+            b // self.__TOTAL
+        )
+
+
 class Zone:
     __lights = ()
 
@@ -6,6 +36,10 @@ class Zone:
         self.__Y0 = y0
         self.__X1 = x1
         self.__Y1 = y1
+        self.__image_handler = ImageHandler(
+            x1 - x0 + 1,
+            y1 - y0 + 1
+        )
 
     # Evaluate equality as true if the zones have the same initial coordinates.
     # Overriding this method is acceptable becuse the "is" operator checks for two of the
@@ -23,3 +57,14 @@ class Zone:
 
     def setLights(self, lights: tuple): self.__lights = lights
     def getLights(self) -> tuple: return self.__lights
+
+    def setImage(self, image: Image):
+        # Assign a cropped image of the screen to the image handler.
+        # The ending coordinates require a +1 as they represent the first pixels that
+        # are not in the crop.
+        self.__image_handler.setImage(
+            image.crop((self.__X0, self.__Y0, self.__X1+1, self.__Y1+1))
+        )
+
+    def getColor(self) -> clr:
+        return self.__image_handler.getAverage()
